e664be18068ded165e9c5f434bbfbe77c254526e
Georgiy Belyanin <belyaningeorge@ya.ru>
2024-08-03 15:46:32 +0300

    Add one more single-source Wikidata query type in the generator

--- Queries/generate-wikidata-sparql-ss-query-sets.sh
+++ Queries/generate-wikidata-sparql-ss-query-sets.sh
@@ -1,5 +1,12 @@
 # The parameters we've used to run the SPARQL Wikidata query generator
+
+# Simple query, usually no more than 1 edge starting from the source.
 python3 ./generate-wikidata-sparql-ss-queries.py "(<http://www.wikidata.org/prop/direct/P131>)*" > "P131*.txt" 192846
+# Simple query but more than 1 edge starting from the source.
+python3 ./generate-wikidata-sparql-ss-queries.py "(<http://www.wikidata.org/prop/direct/P279>)*" > "P279*.txt" 192846
+# More complex query.
 python3 ./generate-wikidata-sparql-ss-queries.py "((<http://www.wikidata.org/prop/direct/P279>|<http://www.wikidata.org/prop/direct/P31>)/((<http://www.wikidata.org/prop/direct/P279>)*|(<http://www.wikidata.org/prop/direct/P31>)*))" > "P279|P31(P279*|P31*)" 192846
+# Limited length query, many state query.
 python3 ./generate-wikidata-sparql-ss-queries.py "(((((<http://www.wikidata.org/prop/direct/P31>/(<http://www.wikidata.org/prop/direct/P279>)?)/(<http://www.wikidata.org/prop/direct/P279>)?)/(<http://www.wikidata.org/prop/direct/P279>)?)/(<http://www.wikidata.org/prop/direct/P279>)?)/(<http://www.wikidata.org/prop/direct/P279>)?)" > "P31 P279? P279? P279? P279? P279?" 192846
+# Searching the siblings.
 python3 ./generate-wikidata-sparql-ss-queries.py "((^<http://www.wikidata.org/prop/direct/P161>/<http://www.wikidata.org/prop/direct/P161>))/((^<http://www.wikidata.org/prop/direct/P161>/<http://www.wikidata.org/prop/direct/P161>))*" > "^P161 P161 (^P161 P161)*"
