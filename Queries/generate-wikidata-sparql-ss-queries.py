0289f4c6d06875882dca26c1e53c451485d18454
Georgiy Belyanin <belyaningeorge@ya.ru>
2024-08-03 11:35:06 +0300

    Add scripts for generating Wikidata SS SPARQL queries

---
+++ Queries/generate-wikidata-sparql-ss-queries.py
@@ -0,0 +1,22 @@
+import sys
+import random
+import datetime
+
+QUERY_COUNT = 1000
+MAX_ENTITY_NUMBER = 107346161
+
+if len(sys.argv) == 1:
+    print("Generates Wikidata SPARQL single-source queries from random entities to stdout")
+    print("Usage: python3 ./generate-wikidata-sparql-queries.py <query>")
+    sys.exit()
+
+seed = 1
+if len(sys.argv) < 3:
+    seed = int(datetime.datetime.now().timestamp())
+else:
+    seed = int(sys.argv[2])
+
+random.seed(seed)
+
+for i in range(1, QUERY_COUNT + 1):
+    print(f'{i},<http://www.wikidata.org/entity/Q{random.randint(1, MAX_ENTITY_NUMBER)}> {sys.argv[1]} ?x1')
---
+++ Queries/generate-wikidata-sparql-ss-query-sets.sh
@@ -0,0 +1,5 @@
+# The parameters we've used to run the SPARQL Wikidata query generator
+python3 ./generate-wikidata-sparql-ss-queries.py "(<http://www.wikidata.org/prop/direct/P131>)*" > "P131*.txt" 192846
+python3 ./generate-wikidata-sparql-ss-queries.py "((<http://www.wikidata.org/prop/direct/P279>|<http://www.wikidata.org/prop/direct/P31>)/((<http://www.wikidata.org/prop/direct/P279>)*|(<http://www.wikidata.org/prop/direct/P31>)*))" > "P279|P31(P279*|P31*)" 192846
+python3 ./generate-wikidata-sparql-ss-queries.py "(((((<http://www.wikidata.org/prop/direct/P31>/(<http://www.wikidata.org/prop/direct/P279>)?)/(<http://www.wikidata.org/prop/direct/P279>)?)/(<http://www.wikidata.org/prop/direct/P279>)?)/(<http://www.wikidata.org/prop/direct/P279>)?)/(<http://www.wikidata.org/prop/direct/P279>)?)" > "P31 P279? P279? P279? P279? P279?" 192846
+python3 ./generate-wikidata-sparql-ss-queries.py "((^<http://www.wikidata.org/prop/direct/P161>/<http://www.wikidata.org/prop/direct/P161>))/((^<http://www.wikidata.org/prop/direct/P161>/<http://www.wikidata.org/prop/direct/P161>))*" > "^P161 P161 (^P161 P161)*"
