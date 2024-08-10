3d9a9c49632c29cd5b30e9a6311b7a870075f341
Georgiy Belyanin <belyaningeorge@ya.ru>
2024-08-10 09:39:42 +0300

    Change generated query sets file names

--- Queries/generate-wikidata-sparql-ss-query-sets.sh
+++ Queries/generate-wikidata-sparql-ss-query-sets.sh
@@ -1,12 +1,12 @@
 # The parameters we've used to run the SPARQL Wikidata query generator
 
 # Simple query, usually no more than 1 edge starting from the source.
-python3 ./generate-wikidata-sparql-ss-queries.py "(<http://www.wikidata.org/prop/direct/P131>)*" > "P131*.txt" 192846
+python3 ./generate-wikidata-sparql-ss-queries.py "(<http://www.wikidata.org/prop/direct/P131>)*" > "wikidata-ss-rs-1.txt" 192846
 # Simple query but more than 1 edge starting from the source.
-python3 ./generate-wikidata-sparql-ss-queries.py "(<http://www.wikidata.org/prop/direct/P279>)*" > "P279*.txt" 192846
+python3 ./generate-wikidata-sparql-ss-queries.py "(<http://www.wikidata.org/prop/direct/P279>)*" > "wikidata-ss-rs-2.txt" 192847
 # More complex query.
-python3 ./generate-wikidata-sparql-ss-queries.py "((<http://www.wikidata.org/prop/direct/P279>|<http://www.wikidata.org/prop/direct/P31>)/((<http://www.wikidata.org/prop/direct/P279>)*|(<http://www.wikidata.org/prop/direct/P31>)*))" > "P279|P31(P279*|P31*)" 192846
+python3 ./generate-wikidata-sparql-ss-queries.py "((<http://www.wikidata.org/prop/direct/P279>|<http://www.wikidata.org/prop/direct/P31>)/((<http://www.wikidata.org/prop/direct/P279>)*|(<http://www.wikidata.org/prop/direct/P31>)*))" > "wikidata-ss-rs-3.txt" 192848
 # Limited length query, many state query.
-python3 ./generate-wikidata-sparql-ss-queries.py "(((((<http://www.wikidata.org/prop/direct/P31>/(<http://www.wikidata.org/prop/direct/P279>)?)/(<http://www.wikidata.org/prop/direct/P279>)?)/(<http://www.wikidata.org/prop/direct/P279>)?)/(<http://www.wikidata.org/prop/direct/P279>)?)/(<http://www.wikidata.org/prop/direct/P279>)?)" > "P31 P279? P279? P279? P279? P279?" 192846
+python3 ./generate-wikidata-sparql-ss-queries.py "(((((<http://www.wikidata.org/prop/direct/P31>/(<http://www.wikidata.org/prop/direct/P279>)?)/(<http://www.wikidata.org/prop/direct/P279>)?)/(<http://www.wikidata.org/prop/direct/P279>)?)/(<http://www.wikidata.org/prop/direct/P279>)?)/(<http://www.wikidata.org/prop/direct/P279>)?)" > "wikidata-ss-rs-4.txt" 192849
 # Searching the siblings.
-python3 ./generate-wikidata-sparql-ss-queries.py "((^<http://www.wikidata.org/prop/direct/P161>/<http://www.wikidata.org/prop/direct/P161>))/((^<http://www.wikidata.org/prop/direct/P161>/<http://www.wikidata.org/prop/direct/P161>))*" > "^P161 P161 (^P161 P161)*"
+python3 ./generate-wikidata-sparql-ss-queries.py "((^<http://www.wikidata.org/prop/direct/P161>/<http://www.wikidata.org/prop/direct/P161>))/((^<http://www.wikidata.org/prop/direct/P161>/<http://www.wikidata.org/prop/direct/P161>))*" > "wikidata-ss-rs-5.txt" 192850
